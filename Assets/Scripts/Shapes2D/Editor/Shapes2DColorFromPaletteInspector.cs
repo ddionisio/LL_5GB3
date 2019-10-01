a8caf83bab855d3c49dcd13cc61868d7c7e78a03
David Dionisio <ddionisio@renegadeware.com>
2019-10-01 16:33:52 -0400

    level background stuff, level 0 stuff

---
+++ Assets/Scripts/Shapes2D/Editor/Shapes2DColorFromPaletteInspector.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(Shapes2DColorFromPalette))]
+public class Shapes2DColorFromPaletteInspector : Editor {
+    SerializedProperty mPalette;
+
+    SerializedProperty mPaletteIndex;
+    SerializedProperty mBrightness;
+    SerializedProperty mAlpha;
+
+    SerializedProperty mPaletteIndex2;
+    SerializedProperty mBrightness2;
+    SerializedProperty mAlpha2;
+
+    SerializedProperty mPaletteIndexOutline;
+    SerializedProperty mBrightnessOutline;
+    SerializedProperty mAlphaOutline;
+
+    void OnDisable() {
+        Undo.undoRedoPerformed -= ApplyColor;
+    }
+
+    void OnEnable() {
+        mPalette = serializedObject.FindProperty("_palette");
+
+        mPaletteIndex = serializedObject.FindProperty("_index");
+        mBrightness = serializedObject.FindProperty("_brightness");
+        mAlpha = serializedObject.FindProperty("_alpha");
+
+        mPaletteIndex2 = serializedObject.FindProperty("_index2");
+        mBrightness2 = serializedObject.FindProperty("_brightness2");
+        mAlpha2 = serializedObject.FindProperty("_alpha2");
+
+        mPaletteIndexOutline = serializedObject.FindProperty("_indexOutline");
+        mBrightnessOutline = serializedObject.FindProperty("_brightnessOutline");
+        mAlphaOutline = serializedObject.FindProperty("_alphaOutline");
+
+        ApplyColor();
+
+        Undo.undoRedoPerformed += ApplyColor;
+    }
+
+    void ApplyColor() {
+        //refresh
+        var dats = serializedObject.targetObjects;
+        for(int i = 0; i < dats.Length; i++) {
+            var dat = (Shapes2DColorFromPalette)dats[i];
+            dat.ApplyColor();
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        //var dat = target as ColorFromPaletteBase;
+
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(mPalette);
+
+        M8.ColorPalette palette = mPalette.objectReferenceValue ? (M8.ColorPalette)mPalette.objectReferenceValue : null;
+
+        DoEdit(palette, mPaletteIndex, mBrightness, mAlpha);
+
+        DoEdit(palette, mPaletteIndex2, mBrightness2, mAlpha2);
+
+        DoEdit(palette, mPaletteIndexOutline, mBrightnessOutline, mAlphaOutline);
+
+        if(serializedObject.ApplyModifiedProperties())
+            ApplyColor();
+    }
+
+    void DoEdit(M8.ColorPalette palette, SerializedProperty ind, SerializedProperty brightness, SerializedProperty alpha) {
+        //Palette Edit
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+
+        if(palette)            
+            EditorGUILayout.IntSlider(ind, 0, palette.count - 1);
+        else
+            EditorGUILayout.PropertyField(ind);
+
+        //Settings
+        EditorGUILayout.Slider(brightness, 0f, 2f);
+
+        EditorGUILayout.Slider(alpha, 0f, 1f);
+
+        EditorGUILayout.EndVertical();
+    }
+}
---
+++ Assets/Scripts/Shapes2D/Shapes2DColorFromPalette.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shapes2DColorFromPalette : M8.ColorFromPaletteBase {
+    
+    [HideInInspector]
+    [SerializeField]
+    int _indexOutline;
+    [HideInInspector]
+    [SerializeField]
+    float _brightnessOutline = 1f;
+    [HideInInspector]
+    [SerializeField]
+    float _alphaOutline = 1f;
+
+    [HideInInspector]
+    [SerializeField]
+    int _index2;
+    [HideInInspector]
+    [SerializeField]
+    float _brightness2 = 1f;
+    [HideInInspector]
+    [SerializeField]
+    float _alpha2 = 1f;
+
+    public Shapes2D.Shape target;
+
+    public int indexOutline {
+        get { return _indexOutline; }
+        set {
+            if(_indexOutline != value) {
+                _indexOutline = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public float brightnessOutline {
+        get { return _brightnessOutline; }
+        set {
+            if(_brightnessOutline != value) {
+                _brightnessOutline = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public float alphaOutline {
+        get { return _alphaOutline; }
+        set {
+            if(_alphaOutline != value) {
+                _alphaOutline = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public Color colorOutline {
+        get {
+            if(palette) {
+                var clr = palette.GetColor(_indexOutline);
+                return new Color(clr.r * _brightnessOutline, clr.g * _brightnessOutline, clr.b * _brightnessOutline, clr.a * _alphaOutline);
+            }
+            else
+                return Color.white;
+        }
+    }
+
+    public int index2 {
+        get { return _index2; }
+        set {
+            if(_index2 != value) {
+                _index2 = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public float brightness2 {
+        get { return _brightness2; }
+        set {
+            if(_brightness2 != value) {
+                _brightness2 = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public float alpha2 {
+        get { return _alpha2; }
+        set {
+            if(_alpha2 != value) {
+                _alpha2 = value;
+                ApplyColor();
+            }
+        }
+    }
+
+    public Color color2 {
+        get {
+            if(palette) {
+                var clr = palette.GetColor(_index2);
+                return new Color(clr.r * _brightness2, clr.g * _brightness2, clr.b * _brightness2, clr.a * _alpha2);
+            }
+            else
+                return Color.white;
+        }
+    }
+
+    public override void ApplyColor() {
+        if(!target)
+            return;
+
+        var setting = target.settings;
+        setting.fillColor = color;
+        setting.fillColor2 = color2;
+        setting.outlineColor = colorOutline;        
+    }
+
+    void Awake() {
+        if(!target)
+            target = GetComponent<Shapes2D.Shape>();
+    }
+}
